0e13d211803ae4c5b6b5bdb17248063912c4dc33
Yash Jaiswal <yashjaiswal1097@gmail.com>
2020-03-27 16:23:16 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# AdversarialAttacks
+
+BTP Thesis Project - Adversarial Attacks on Neural Networks
+
+Our team developed the model under the Mentorship of Dr.Saumya Bhadauria and Dr.Yash Daultani to create an adversarial attack on the existing
+deep learning models by modifying input image such that it
+will be classified incorrectly.
+
+We also Suggested the defenses against these adversarial attacks
+inorder to make the model robust so that it can classify perturbed
+images correctly
---
+++ WhiteBoxAttacks/targetedAndNontargeted.py
@@ -0,0 +1,40 @@
+def non_targeted_attack(img):
+    img = img.cuda()
+    label = torch.zeros(1, 1).cuda()
+
+    x, y = Variable(img, requires_grad=True), Variable(label)
+    for step in range(steps):
+        zero_gradients(x)
+        out = model(x)
+        y.data = out.data.max(1)[1]
+        _loss = loss(out, y)
+        _loss.backward()
+        normed_grad = step_alpha * torch.sign(x.grad.data)
+        step_adv = x.data + normed_grad
+        adv = step_adv - img
+        adv = torch.clamp(adv, -eps, eps)
+        result = img + adv
+        result = torch.clamp(result, 0.0, 1.0)
+        x.data = result
+    return result.cpu(), adv.cpu()
+
+
+
+def targeted_attack(img, label_idx):
+    img = img.cuda()
+    label = torch.Tensor([label_idx]).long().cuda()
+
+    x, y = Variable(img, requires_grad=True), Variable(label)
+    for step in range(steps):
+        zero_gradients(x)
+        out = model(x)
+        _loss = loss(out, y)
+        _loss.backward()
+        normed_grad = step_alpha * torch.sign(x.grad.data)
+        step_adv = x.data - normed_grad
+        adv = step_adv - img
+        adv = torch.clamp(adv, -eps, eps)
+        result = img + adv
+        result = torch.clamp(result, 0.0, 1.0)
+        x.data = result
+    return result.cpu(), adv.cpu()
